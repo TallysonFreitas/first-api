ce3307cd3ffec2f9c940f3a0c69c9e8816faeef4
Tallyson <tallyson.eu@outlook.com>
2024-06-20 14:40:50 -0300

    feat: cria rota para cadastrar novos usuarios

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from config import config
 
 app = Flask(__name__)
 
+# Route to get a User
 @app.route('/get-user/<user_id>')
 def get_user(user_id):
     # connection = psycopg2.connect(host='localhost', port='5432', database='store', user='postgres', password='admin')
     user_data = {
             "user_id": user_id,
-            "name": "tallyson",
-            "email": "tallyson.eu@outlook.com"
+            "name": None,
+            "email": None
         }
 
     try:
@@ -19,7 +20,7 @@ def get_user(user_id):
         params = config()
         connection = psycopg2.connect(**params)
         crsr = connection.cursor()
-        crsr.execute('SELECT * FROM users WHERE id='+user_id)
+        crsr.execute(f'SELECT * FROM users WHERE id={user_id};')
         user = crsr.fetchone()
         user_data['email'] = user[2]
         user_data['name'] = user[1]
@@ -33,6 +34,36 @@ def get_user(user_id):
 
     return jsonify(user_data),200
 
+# Route to send a new User
+@app.route('/create-user/<username>/<email>')
+def create_user(username,email):
+    try:
+        connection = None
+        params = config()
+        connection = psycopg2.connect(**params)
+        crsr = connection.cursor()
+        crsr.execute("""
+            INSERT INTO users (first_name, email)
+            VALUES (%s, %s);
+            """,
+            (username,email))
+        crsr.execute(f'SELECT * FROM users WHERE id > 1000;')
+        crsr.fetchone()
+
+        # Make the changes persist in the database
+        connection.commit()
+
+        # Close comunication with the database
+        crsr.close()
+        connection.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return '404'
+    finally:
+        if connection is not None:
+            connection.close()
+
+    return '200'
 
 if __name__ == '__main__':
     app.run(debug=True)
