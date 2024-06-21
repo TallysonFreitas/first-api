ea1034c3f3d37b28df1b7826bc2a6602de56d816
Tallyson <tallyson.eu@outlook.com>
2024-06-21 11:09:46 -0300

    fix: resolve problema de cors

--- main.py
+++ main.py
@@ -65,5 +65,12 @@ def create_user(username,email):
 
     return '200'
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
